a31037577a568c6541709eda58caf5c34b5f8973
Antonio Robles <antjrobles@gmail.com>
2023-03-03 11:33:35 +0100

    BARBAR keymaps

--- nvim/lua/plugins_configs.lua
+++ nvim/lua/plugins_configs.lua
@@ -20,50 +20,78 @@ vim.g.indent_colors = {fg = '#5c6370', bg = '#282c34'}
 
 -- BARBAR --
 local map = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
+local opts = {
+    noremap = true,
+    silent = true,
+    }
 
--- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)        
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)   
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
--- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
--- Magic buffer-picking mode
-map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
--- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+require'bufferline'.setup {
+    animation = true,                                             -- Enable/disable animations
+    auto_hide = false,                                            -- Enable/disable auto-hiding the tab bar when there is a single buffer
+    tabpages = true,                                              -- Enable/disable tabpages 
+    closable = true,                                              -- Enables/disable close button
+    clickable = true,                                             -- Enables/disables clickable tabs 
+    diagnostics = {                                               -- Options for diagnostics (or indicator errors, warnings, info)
+    -- you can use a list
+    {enabled = true, icon = 'ﬀ'}, -- ERROR
+    {enabled = false}, -- WARN
+    {enabled = false}, -- INFO
+    {enabled = true},  -- HINT
+
+    -- OR `vim.diagnostic.severity`
+    [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+    [vim.diagnostic.severity.WARN] = {enabled = false},
+    [vim.diagnostic.severity.INFO] = {enabled = false}, 
+    [vim.diagnostic.severity.HINT] = {enabled = true},
+  },
+    highlight_alternate = false,                                  -- Enable 
+    highlight_inactive_file_icons = false,                        -- 
+    highlight_visible = true,
+    icons = true, 
 
 
+    on_separator_active = '▎',
+    icon_separator_inactive = '▎',
+    icon_close_tab = '',
+    icon_close_tab_modified = '●',
+    icon_pinned = '車',
 
 
--- NERD TREE --
+
+    minimum_padding = 3,                                        -- Minimum padding width at the ends of the bufferline 
+    maximum_length = 30,                                        -- Maximum buffer name length
+    letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',    -- Set letters for each buffer
+    no_name_title = nil,                                        -- 
+}
+
+
+
+
+
+-- NVIM TREE --
 -- empty setup using defaults
 require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
-    view = {width = 30, mappings = {list = {{key = "u", action = "dir_up"}}}},
+    view = {
+        width = 40, 
+        mappings = {
+            list = {
+                {key = "u", action = "dir_up"}
+            }
+        }
+    },
     renderer = {group_empty = true},
-    filters = {dotfiles = true}
+    filters = {dotfiles = false},
+    update_cwd = true,
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {}
+    },
+
 })
 
 
